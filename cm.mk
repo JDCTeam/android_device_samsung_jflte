b61f2c1f22dba739193fa897b91c6a31d121cf23
Mattia D'Alleva <antaresone@antaresone.eu>
2015-05-22 02:48:42 +0200

    Makefile: inherit JDC common.mk

--- cm.mk
+++ cm.mk
@@ -6,4 +6,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# Inherit some common JDC stuff.
+$(call inherit-product, vendor/jdc/config/common.mk)
+
 PRODUCT_NAME := cm_jflte
