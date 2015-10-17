683da6d3cf850bbd6b4815e2fce2e6adbbde26d1
Mattia D'Alleva <antaresone@antaresone.eu>
2015-10-17 17:00:10 +0200

    init: build vendor init as C++
    Required by https://github.com/JDCTeam/android_system_core/commit/e0e1f61a3a8a12691046ac7e642873edf82f4886

--- init/Android.mk
+++ init/Android.mk
@@ -5,6 +5,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
-LOCAL_SRC_FILES := init_jflte.c
+LOCAL_SRC_FILES := init_jflte.cpp
 LOCAL_MODULE := libinit_jflte
 include $(BUILD_STATIC_LIBRARY)
