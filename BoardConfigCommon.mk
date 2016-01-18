619b0a35e958502bdb269f97c678850d561bccd4
Mattia D'Alleva <antaresone@antaresone.eu>
2016-01-18 23:46:45 +0100

    BoardConfig: drop OTA assert devices
    Moved to device_samsung_jflte

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -28,9 +28,6 @@ COMMON_PATH := device/samsung/jf-common
 # inherit from the proprietary version
 -include vendor/samsung/jf-common/BoardConfigVendor.mk
 
-# Assert
-TARGET_OTA_ASSERT_DEVICE := jflte,jfltexx,i9505,GT-I9505,jgedlte,i9505g,GT-I9505G,jfltevzw,jfltespr,jflterefreshspr,jfltetmo,jfltecri,jfltecsp,jflteatt,jfltecan,jfltetfnatt,jfltetfntmo,jflteusc,jfltezm
-
 # Platform
 TARGET_BOARD_PLATFORM := msm8960
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno320
@@ -143,7 +140,7 @@ USE_DEVICE_SPECIFIC_DATASERVICES := true
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
-    
+
 # Vendor Init
 TARGET_UNIFIED_DEVICE := true
 TARGET_INIT_VENDOR_LIB := libinit_jflte
