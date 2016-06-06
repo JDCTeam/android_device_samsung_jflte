05193847ff82be7facfb61aca0393e8a0030b4bf
Dimitris Katikaridis <dkatikaridis@gmail.com>
2016-06-07 01:59:21 +0300

    REALLY??what a typo
    Change-Id: Iaf4b9ba4a311874789eaff918922982e6abfc535

--- jf-common.mk
+++ jf-common.mk
@@ -79,7 +79,7 @@ PRODUCT_PACKAGES += \
     libaudio-resampler
 
 # Audio configuration
-PRODUCT_COPY_FILES += \ 
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
