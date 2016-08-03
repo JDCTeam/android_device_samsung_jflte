f044354019ca77c5aa8aef2e5110a6e9ea3a6273
Grando F <aqdevmain@gmail.com>
2016-08-03 11:42:44 -0700

    jf-common: Build libstlport
    This is needed by the Adreno blobs that we are using from flo.
    Moving it up from some devices that were using it for RIL already.
    
    Change-Id: I40fe8f7824e24b7e718c41f9c33153796e1ffa9f

--- jf-common.mk
+++ jf-common.mk
@@ -201,6 +201,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     device/samsung/jf-common/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
 
+# Stlport
+PRODUCT_PACKAGES += \
+    libstlport
+
 # Wifi
 PRODUCT_PACKAGES += \
     libnetcmdiface \
