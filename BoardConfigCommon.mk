4d155e4f74d54de2ba950fd9dd817c07552b026c
Dimitris Katikaridis <dkatikaridis@gmail.com>
2016-08-22 18:33:00 +0300

    Webview: use beta version
    Originally took from https://github.com/AOSP-JF-MM/platform_device_samsung_jflte/commit/e65d49bf1bd6f5a3989654010db84163e47cfc76
    
    Change-Id: I403a6692fbeb7facec0a5d92baf33a561aab0a48

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -156,6 +156,10 @@ BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 BOARD_VOLD_MAX_PARTITIONS := 28
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
+# Webview version
+PREBUILT_WEBVIEW_VERSION := beta
+
+
 # Wifi module
 BOARD_WLAN_DEVICE := bcmdhd
 BOARD_HAVE_SAMSUNG_WIFI := true
