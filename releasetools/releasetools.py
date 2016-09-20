f4efb5d760c8be98c0bc3717ff9fa6d19ecd9640
B--B <dark.schneider.ds@gmail.com>
2016-09-20 19:14:10 +0200

    Unified device: Fix multi device support (PART I)
    This commit fully fixes GSM blobs for all variants
    CDMA/USC not tested (may work, but probably need some small changes)
    VZW not tested (but i'm pretty sure it's still unbootable)

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -38,7 +38,13 @@ def FullOTA_CustomAsserts(info):
   info.script.AppendExtra('delete_recursive("/system/rild");')
 
 def FullOTA_InstallEnd(info):
+  info.script.Mount("/system")
+  info.script.AppendExtra('set_metadata("/system/bin/qcks", "uid", 0, "gid", 2000, "mode", 0755, "capabilities", 0x0, "selabel", "u:object_r:mdm_helper_exec:s0");')
+  info.script.AppendExtra('set_metadata("/system/bin/ks", "uid", 0, "gid", 2000, "mode", 0755, "capabilities", 0x0, "selabel", "u:object_r:mdm_helper_exec:s0");')
+  info.script.AppendExtra('set_metadata("/system/bin/netmgrd", "uid", 0, "gid", 2000, "mode", 0755, "capabilities", 0x0, "selabel", "u:object_r:netmgrd_exec:s0");')
+  info.script.AppendExtra('set_metadata("/system/bin/qmuxd", "uid", 0, "gid", 2000, "mode", 0755, "capabilities", 0x0, "selabel", "u:object_r:qmuxd_exec:s0");')
   info.script.script = [cmd for cmd in info.script.script if not "boot.img" in cmd]
   info.script.script = [cmd for cmd in info.script.script if not "show_progress(0.100000, 0);" in cmd]
   info.script.AppendExtra('package_extract_file("boot.img", "/tmp/boot.img");')
   info.script.AppendExtra('assert(run_program("/sbin/sh", "/system/etc/loki.sh") == 0);')
+  info.script.Unmount("/system")
