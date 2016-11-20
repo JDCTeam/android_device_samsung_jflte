766ee1654b6cd5f4a7689eb1f693692dd1e6e090
Gabriele M <moto.falcon.git@gmail.com>
2016-11-20 20:04:15 +0100

    jf: Generate graphics vendor Makefile if missing
    This allows to build CM without cloning vendor repos.
    
    Change-Id: I87777485edc5f29c5899091176e55d28e36bb263

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -39,7 +39,11 @@ write_headers "jactivelte jflteatt jfltespr jfltetmo jfltevzw jfltexx jfltecan j
 
 write_makefiles "$MY_DIR"/common-proprietary-files.txt
 
-printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/msm8960/graphics/graphics-vendor.mk)" >> "$PRODUCTMK"
+if [ -s "$CM_ROOT"/vendor/qcom/binaries/msm8960/graphics/graphics-vendor.mk ]; then
+    printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/msm8960/graphics/graphics-vendor.mk)" >> "$PRODUCTMK"
+else
+    write_makefiles "$MY_DIR"/../../qcom/common/extractors/graphics-msm8960.txt
+fi
 
 write_footers
 
