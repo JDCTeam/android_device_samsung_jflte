25c35ea276f6023df0791bce2a29baf8456b9176
B--B <dark.schneider.ds@gmail.com>
2016-11-20 17:30:23 +0100

    Revert "bluetooth: Our kernel is missing CLOCK_BOOTTIME_ALARM (alarmtimer)"
    This reverts commit 3b5c81a9f88be618cb02c2f70f22a98a12e7bc6b.
    
    We are using kernel/time/alarmtimer.c in Alucard kernel
    
    This commit hopefully fixes some random BT crashes experienced by our testers/users
    
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -25,6 +25,4 @@
 #define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
 #define BTIF_HF_WBS_PREFERRED TRUE  /* Use WBS    */
 
-/* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
-#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
 #endif
