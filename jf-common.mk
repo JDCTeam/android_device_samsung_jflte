4eae6a2f4a63aa2185c2f073586946c3bc2afff6
Pawin Vongmasa <pawin@google.com>
2017-05-03 19:54:12 -0700

    jf: Create media_profiles_V1_0.xml
    * media_profiles_V1_0.xml is created from media_profiles.xml by replacing
      the dtd schema inside media_profiles.xml with an external reference to
      /system/etc/media_profiles_V1_0.dtd.
    
    Change-Id: I0fbb03603a72d0108a96bfa0f0a4ceb12ef3d430

--- jf-common.mk
+++ jf-common.mk
@@ -146,7 +146,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 # NFC packages
 PRODUCT_PACKAGES += \
