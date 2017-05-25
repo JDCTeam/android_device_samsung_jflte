83fe29b276de8e059d576b265c5610f84e7e57f6
dkati <dkatikaridis@gmail.com>
2017-05-25 21:05:50 +0300

    jf:Use the beta version of webview
    Change-Id: I899f36513fd10d4e9274767ca74ba8acda9de7a2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -161,7 +161,7 @@ BOARD_VOLD_MAX_PARTITIONS := 28
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
 # Webview version
-PREBUILT_WEBVIEW_VERSION := stable
+PREBUILT_WEBVIEW_VERSION := beta
 
 # Wifi module
 BOARD_WLAN_DEVICE := bcmdhd
