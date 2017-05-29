d105643c12d12e57a7424a71bf81627a3f8aac93
gtpitch <bmontagu@gmail.com>
2017-05-28 22:50:29 -0400

    JF: doze: Adapt for AOSP
    Change-Id: I2429f3a04bbb6e5e1268da2afe3ac382ee30ae1e
    Signed-off-by: gtpitch <bmontagu@gmail.com>

--- doze/src/com/android/settings/device/SamsungDozeService.java
+++ doze/src/com/android/settings/device/SamsungDozeService.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.android.settings.device;
 
 import android.app.Service;
 import android.content.BroadcastReceiver;
