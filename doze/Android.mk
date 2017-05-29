3d0c7bf5a5d21dfce8ea78963197e55fa11b74f2
gtpitch <bmontagu@gmail.com>
2017-05-28 22:50:29 -0400

    JF: doze: Adapt for AOSP
    Change-Id: I2429f3a04bbb6e5e1268da2afe3ac382ee30ae1e
    Signed-off-by: gtpitch <bmontagu@gmail.com>

--- doze/Android.mk
+++ doze/Android.mk
@@ -1,9 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-        org.cyanogenmod.platform.internal
-
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
@@ -16,8 +13,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v14-preference \
     android-support-v7-appcompat \
     android-support-v7-preference \
-    android-support-v7-recyclerview \
-    org.cyanogenmod.platform.internal
+    android-support-v7-recyclerview
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
