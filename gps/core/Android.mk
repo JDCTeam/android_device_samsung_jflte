d04b2aae65652993ccf1c7750987925ac0b86208
Dante Russo <drusso@codeaurora.org>
2017-06-27 02:59:41 +0200

    jf-common: gps: Removing header copying to out folder
    Replace copying header files to out folder
    for use by other modules to instead build
    headers into separate libraries that end
    with _headers that can be used by other
    modules.
    
    Change-Id: I073967cd34e5c79110895908a4eef1b7d17bf9ea
    CRs-fixed: 2068129

--- gps/core/Android.mk
+++ gps/core/Android.mk
@@ -30,20 +30,12 @@ LOCAL_CFLAGS += \
      -Wno-error \
      -Wno-unused-parameter
 
-LOCAL_C_INCLUDES:= \
-    $(TARGET_OUT_HEADERS)/gps.utils
-
-LOCAL_COPY_HEADERS_TO:= libloc_core/
-LOCAL_COPY_HEADERS:= \
-    MsgTask.h \
-    LocApiBase.h \
-    LocAdapterBase.h \
-    ContextBase.h \
-    LocDualContext.h \
-    IzatProxyBase.h \
-    UlpProxyBase.h \
-    gps_extended_c.h \
-    gps_extended.h \
-    loc_core_log.h
+LOCAL_HEADER_LIBRARIES := \
+    libgps.utils_headers
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libloc_core_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+include $(BUILD_HEADER_LIBRARY)
