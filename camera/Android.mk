8e849d615519c05a16e0b62ebe4567c6af08dd26
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-08-24 02:56:36 +0200

    jf: camera: Update for 8.1 [1/2]
    * CameraParametersExtra.h in frameworks/av no longer exists.
    * Add build dependencies.
    
    Change-Id: If9de1da3af326cb53674a3c9536ca9bfea2a4351
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- camera/Android.mk
+++ camera/Android.mk
@@ -8,7 +8,11 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils
+    libhardware liblog libcamera_client libutils \
+    android.hardware.graphics.bufferqueue@1.0 \
+    android.hidl.token@1.0-utils
+
+LOCAL_STATIC_LIBRARIES := libarect libbase
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := camera.msm8960
