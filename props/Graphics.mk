c6aff77063d54d7f8906f300538726835a1f58d3
Nirmal Abraham <nabrah@codeaurora.org>
2017-10-10 13:51:47 +0530

    jf-common: Disable SF Latch Buffer Signaled property
    Change-Id: I682b30fbefdfbd4d8478a82148d5984138078957

--- props/Graphics.mk
+++ props/Graphics.mk
@@ -4,7 +4,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	debug.composition.type=c2d \
     debug.hwui.use_buffer_age=false \
     debug.sf.enable_gl_backpressure=1 \
-    ro.hwui.text_large_cache_height=2048
+    ro.hwui.text_large_cache_height=2048 \
+    debug.sf.latch_unsignaled=1
 
 # Experimental
 #PRODUCT_PROPERTY_OVERRIDES += \
