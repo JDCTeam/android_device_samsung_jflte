6a56fb5406622f83782a1952ff9711a0b1c366de
dkati <dkatikaridis@gmail.com>
2017-10-22 01:29:14 +0300

    From CM to LineageOS
    Change-Id: Ib723affdce3ba1804e8403b1685616b727e4000f

--- lineage.mk
+++ lineage.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/samsung/jflte/full_jflte.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit some common JDC stuff.
 $(call inherit-product, vendor/jdc/config/common.mk)
 
-PRODUCT_NAME := cm_jflte
+PRODUCT_NAME := lineage_jflte
