70bb45fb88d7a832e239aaaf026364c957d568f0
dkati <dkatikaridis@gmail.com>
2017-10-22 12:43:24 +0300

    init:Use libbase for init logs
    Change-Id: I1481699763ef4aa59bf46b20f65a49b3ee5cc76b

--- init/Android.mk
+++ init/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
+LOCAL_STATIC_LIBRARIES := libbase
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_jflte.cpp
 LOCAL_MODULE := libinit_jflte
