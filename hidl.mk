28e5d9d82f1f1dbc4c5d1853e5f9ea5d36302c0b
dkati <dkatikaridis@gmail.com>
2017-10-28 15:45:56 +0300

    hidl:Fix camera hidl name
    Change-Id: I054437635f03fe423f3c55697bf7cda3c3cb258e

--- hidl.mk
+++ hidl.mk
@@ -26,7 +26,7 @@ PRODUCT_PACKAGES += \
 # Camera HIDL
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    camera.device@1.0-impl.legacy
+    camera.device@1.0-impl-legacy
 
 # Display HIDL
 PRODUCT_PACKAGES += \
