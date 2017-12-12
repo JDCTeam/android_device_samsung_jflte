181abdb0db20630a4f9bcdca1591f5d3fa84ba76
dkati <dkatikaridis@gmail.com>
2017-12-12 13:36:13 +0200

    init:add missing libselinux lib
    Also remove cm dependencies
    
    Change-Id: I7dd6d521ddab7b9a4da3712b52477556c6beb65f

--- init/Android.mk
+++ init/Android.mk
@@ -7,5 +7,5 @@ LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_jflte.cpp
 LOCAL_MODULE := libinit_jflte
-LOCAL_STATIC_LIBRARIES := libbase
+LOCAL_STATIC_LIBRARIES := libbase libselinux
 include $(BUILD_STATIC_LIBRARY)
