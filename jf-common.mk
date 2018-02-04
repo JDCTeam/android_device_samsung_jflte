8109a4759d76845ec0d979a11134252dba0c717c
Arne Coucheron <arco68@gmail.com>
2018-02-04 07:15:51 +0100

    jf-common: Implement custom power HIDL with support for power profiles
    Change-Id: I1d241498e8d7c8767db015aa7cd7250415b1e2ed

--- jf-common.mk
+++ jf-common.mk
@@ -208,7 +208,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.1-service.jf
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
