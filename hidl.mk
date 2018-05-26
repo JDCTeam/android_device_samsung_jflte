5a7b940d8e2ee6e61e228cf45c97aa83b513b5cb
dkati <dkatikaridis@gmail.com>
2018-05-26 18:16:19 +0300

    jf:hidl:Edit the camera's hidl package
    Change-Id: Ifb0566776c95b1024739b5a49a4e13affa667789

--- hidl.mk
+++ hidl.mk
@@ -25,8 +25,8 @@ PRODUCT_PACKAGES += \
 
 # Camera HIDL
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
-    camera.device@1.0-impl-legacy
+    android.hardware.camera.provider@2.4-impl.legacy \
+    camera.device@1.0-impl.legacy
 
 # Display HIDL
 PRODUCT_PACKAGES += \
