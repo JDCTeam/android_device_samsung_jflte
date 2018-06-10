7caae0c4a7aff91688d8eadcc60a9c232835973f
dkati <dkatikaridis@gmail.com>
2018-06-10 14:27:44 +0300

    jf: Declare the arch and CPU variants
    Change-Id: I904e428ac6c22025b3ba9183bf324996a7b6e943

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -32,6 +32,10 @@ TARGET_BOARD_PLATFORM := msm8960
 
 # Architecture
 TARGET_CPU_VARIANT := krait
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := MSM8960
