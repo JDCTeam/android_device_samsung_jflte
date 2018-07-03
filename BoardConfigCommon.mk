d10909dcb44021da5a24edd47097d13ddff28cf3
dkati <dkatikaridis@gmail.com>
2018-07-03 16:26:12 +0300

    jf:Use legacy sepolicy too
    Change-Id: Iff86ba69c243932c5926cc5e763cb9e1c4e43302

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -160,7 +160,9 @@ BOARD_PROVIDES_LIBRIL := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
+BOARD_SECCOMP_POLICY += $(BOARD_PATH)/seccomp
 
 # SU
 WITH_SU := true
