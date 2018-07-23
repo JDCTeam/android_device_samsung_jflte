fba91eca8011fecf798e25c6c3234000d4703612
dkati <dkatikaridis@gmail.com>
2018-07-23 20:54:42 +0300

    jflte:Add vidc firmware
    Change-Id: I2ed965901ac820a3b3a2072f3201b8fb8867d2af

--- Android.mk
+++ Android.mk
@@ -24,6 +24,22 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter jactivelte jflte,$(TARGET_DEVICE)),)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
+
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+
+# Create /firmware links
+VIDC_IMAGES := \
+    vidc.b00 vidc.b01 vidc.b02 vidc.b03 vidc.mdt
+
+VIDC_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(VIDC_IMAGES)))
+$(VIDC_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VIDC_SYMLINKS)
 
 endif
