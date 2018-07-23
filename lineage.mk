e9734c04b5bd51ddac2bf30d151e1d7336b8cf99
dkati <dkatikaridis@gmail.com>
2018-07-23 21:00:15 +0300

    jflte:Add vendor props and fingerprint
    Change-Id: Ica87db63fe221d0389f1dd2d001964728678c4a9

--- lineage.mk
+++ lineage.mk
@@ -7,3 +7,16 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, vendor/jdc/config/common.mk)
 
 PRODUCT_NAME := lineage_jflte
+
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+TARGET_VENDOR_PRODUCT_NAME := GalaxyS4
+TARGET_VENDOR_DEVICE_NAME := GalaxyS4
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="jfltexx-user 5.0.1 LRX22C I9505XXUHQK1 release-keys"
+
+BUILD_FINGERPRINT := samsung/jfltexx/jflte:5.0.1/LRX22C/I9505XXUHQK1:user/release-keys
+
+TARGET_VENDOR := Samsung
