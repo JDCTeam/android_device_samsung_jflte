a50f43b8f2bf088cc8a3f0ca7bdf884f91043319
dkati <dkatikaridis@gmail.com>
2018-08-18 23:01:53 +0300

    props:Add some display props from stock
    Change-Id: I6932b503513b647c380e78b64f6221df617f09a9

--- system_prop.mk
+++ system_prop.mk
@@ -80,6 +80,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.hwui.use_buffer_age=false \
     debug.composition.type=c2d \
     ro.opengles.version=196608 \
+    ro.qualcomm.cabl=0 \
+    ro.hwui.text_large_cache_height=2048 \
+    persist.debug.wfd.enable=1 \
+    persist.sys.wfd.virtual=0 \
     ro.sf.lcd_density=420
 
 # GPS
