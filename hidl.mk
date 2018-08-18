2c9a1022da1ca77ae1ef5416c92cc96f972c1323
dkati <dkatikaridis@gmail.com>
2018-08-18 23:57:34 +0300

    hidl:camera:get out for -legacy tag
    Change-Id: I23c84cf367d72ace059851639d60c94a4aebf749

--- hidl.mk
+++ hidl.mk
@@ -27,8 +27,8 @@ PRODUCT_PACKAGES += \
 
 # Camera HIDL
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
-    camera.device@1.0-impl-legacy
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl
 
 # Configstore
 PRODUCT_PACKAGES += \
