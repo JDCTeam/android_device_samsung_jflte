3682d2e22396f80ee0c1219f65e78cf401f1a7da
dkati <dkatikaridis@gmail.com>
2018-10-24 00:04:10 +0300

    prop:Do not wait for card

--- system_prop.mk
+++ system_prop.mk
@@ -3,8 +3,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so \
     ro.com.android.mobiledata=false \
     persist.radio.add_power_save=1 \
-    persist.radio.apm_sim_not_pwdn=1 \
-    persist.radio.no_wait_for_card=1
+    persist.radio.apm_sim_not_pwdn=1
 
 # GPS
 PRODUCT_PROPERTY_OVERRIDES += \
