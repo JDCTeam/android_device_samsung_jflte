a88a956fe12ff9e04d79c550150cf56d7f9db208
dkati <dkatikaridis@gmail.com>
2018-10-27 11:29:23 +0300

    prop: Disable voip pcm

--- system_prop.mk
+++ system_prop.mk
@@ -50,7 +50,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.fluence.voicecall=true \
     ro.vendor.audio.sdk.fluencetype=fluence \
     vendor.dedicated.device.for.voip=true \
-    vendor.voice.path.for.pcm.voip=true
+    vendor.voice.path.for.pcm.voip=false
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
