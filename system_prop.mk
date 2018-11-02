e309b5100ab34d95506b77a2bcfa6051ee3fe692
dkati <dkatikaridis@gmail.com>
2018-11-02 14:25:23 +0200

    prop: Enable radio.no_wait_for_card

--- system_prop.mk
+++ system_prop.mk
@@ -3,7 +3,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so \
     ro.com.android.mobiledata=false \
     persist.radio.add_power_save=1 \
-    persist.radio.apm_sim_not_pwdn=1
+    persist.radio.apm_sim_not_pwdn=1 \
+    persist.radio.no_wait_for_card=1
 
 # Dex
 PRODUCT_PROPERTY_OVERRIDES += \
