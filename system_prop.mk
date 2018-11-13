f09e4ffd7383c7eb6aedb191523ddf1adca789cd
dkati <dkatikaridis@gmail.com>
2018-11-14 01:08:54 +0200

    prop: Enable qc_nlp_in_use

--- system_prop.mk
+++ system_prop.mk
@@ -22,7 +22,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     
 # GPS
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.gps.qc_nlp_in_use=0 \
+    persist.gps.qc_nlp_in_use=1 \
     ro.gps.agps_provider=1
 
 # Media
