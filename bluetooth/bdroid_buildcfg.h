9889d8065b3154a77e1e553a36b1a750dd0c1b88
Arne Coucheron <arco68@gmail.com>
2018-11-25 03:14:23 +0100

    Update bluetooth config

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -22,11 +22,12 @@
 #define BTM_DEF_LOCAL_NAME   "Samsung Galaxy S IV"
 
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
-//#define BLE_VND_INCLUDED   TRUE
-#define BLE_PRIVACY_SPT    FALSE
-#define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
-#define BTIF_HF_WBS_PREFERRED FALSE  /* Use WBS    */
-
-#define BTIF_HF_CLIENT_WBS_INCLUDED TRUE
+//#define BLE_VND_INCLUDED TRUE
+#define BLE_PRIVACY_SPT FALSE
+#define SC_MODE_INCLUDED FALSE
+#define SDP_DEBUG FALSE
+#define BTSNOOP_MEM FALSE
+#define BTIF_HF_WBS_PREFERRED TRUE /* Use WBS */
+#define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
 
 #endif
