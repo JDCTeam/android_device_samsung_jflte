9a094ec6d6592c68d7a61fc3afc982849060409a
dkati <dkatikaridis@gmail.com>
2018-12-14 00:54:33 +0200

    Android.mk: Ship vidc firmware images and it's symlinks

--- Android.mk
+++ Android.mk
@@ -63,4 +63,16 @@ $(FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_SYMLINKS)
 
+VIDC_IMAGES := \
+    vidc.b00 vidc.b01 vidc.b02 vidc.b03 vidc.mdt
+
+VIDC_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(VIDC_IMAGES)))
+$(VIDC_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+    @echo "Firmware link: $@"
+    @mkdir -p $(dir $@)
+    @rm -rf $@
+    $(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VIDC_SYMLINKS)
+
 endif
