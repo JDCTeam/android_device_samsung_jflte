f46be61d666466a1603aed0e1126edd92ca1e501
dkati <dkatikaridis@gmail.com>
2018-12-14 00:05:59 +0200

    jf: Update camera for Pie
    * Update the wrapper
    * Expose the camera features
    * Enable the proper flags

--- camera/Android.mk
+++ camera/Android.mk
@@ -2,20 +2,21 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES += \
+    framework/native/include \
     system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils \
-    libgui android.hidl.token@1.0-utils \
+    libhardware liblog libcamera_client libgui \
+    libhidltransport libutils android.hidl.token@1.0-utils
 
-LOCAL_STATIC_LIBRARIES := libarect libbase
+LOCAL_STATIC_LIBRARIES := libarect
 
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE := camera.msm8960
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
-LOCAL_PROPRIETARY_MODULE := true
+LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
