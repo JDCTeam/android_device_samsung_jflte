bf01ba48d1d2b88254bb6a2c9206cc86a8f67e6f
dkati <dkatikaridis@gmail.com>
2018-12-14 01:04:50 +0200

    jf-common: manifest: Set memtrack to hwbinder

--- jf-common.mk
+++ jf-common.mk
@@ -114,6 +114,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     copybit.msm8960 \
     gralloc.msm8960 \
     hwcomposer.msm8960 \
