cdfaa1ecbd4feae65ee03952765b337d947360ae
dkati <dkatikaridis@gmail.com>
2018-12-14 01:45:58 +0200

    BoardConfig: Remove deprecated CPU and ARCH flags

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -36,10 +36,6 @@ TARGET_BOARD_PLATFORM := msm8960
 
 # Architecture
 TARGET_CPU_VARIANT := krait
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
 
 # Binder
 TARGET_USES_64_BIT_BINDER := true
