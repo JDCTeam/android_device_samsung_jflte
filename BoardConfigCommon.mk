900101bf4acb655795c979983f10a801c204956e
dkati <dkatikaridis@gmail.com>
2018-12-14 02:00:02 +0200

    BoardConfig: Stop using sdclang

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -170,9 +170,6 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)/releasetools
 BOARD_PROVIDES_LIBRIL := true
 TARGET_RIL_VARIANT := caf
 
-# SDClang
-TARGET_USE_SDCLANG := true
-
 # SELinux
 #include device/qcom/sepolicy/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
