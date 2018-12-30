380a0a978a6cb745afcba401ee68490446775e35
Buff99 <buffbuffster@gmail.com>
2018-12-30 07:27:01 +0000

    JF:defconfig - change to buff kernel defconfig (for now)

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -67,7 +67,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 TARGET_KERNEL_SOURCE := kernel/samsung/jf
-TARGET_KERNEL_CONFIG := side_jf_defconfig
+TARGET_KERNEL_CONFIG := buff_jf_defconfig
 
 # Toolchain
 KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/gcc/linux-x86/arm/arm-linux-7.1.x-gnueabi/bin
