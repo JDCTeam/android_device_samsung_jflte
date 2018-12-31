3ce5f02a27c301fe65f483f2f29fb3d147b34ae4
dkati <dkatikaridis@gmail.com>
2018-12-31 20:13:20 +0200

    CameraWrapper: Redefine exposure compensations and step

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -140,6 +140,11 @@ static char* camera_fixup_getparams(int id, const char* settings) {
                "auto,asd,action,portrait,landscape,night,night-portrait,theatre,beach,snow,sunset,"
                "steadyphoto,fireworks,sports,party,candlelight,backlight,flowers,AR");
 
+    /* Exposure */
+    params.set(android::CameraParameters::KEY_EXPOSURE_COMPENSATION_STEP, "0.5");
+    params.set(android::CameraParameters::KEY_MIN_EXPOSURE_COMPENSATION, "-4");
+    params.set(android::CameraParameters::KEY_MAX_EXPOSURE_COMPENSATION, "4");
+
 #if !LOG_NDEBUG
     ALOGV("%s: fixed parameters:", __FUNCTION__);
     params.dump();
