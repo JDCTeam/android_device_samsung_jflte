ecf1594c723a57052591555806993174c2e7c719
dkati <dkatikaridis@gmail.com>
2019-01-28 22:45:04 +0200

    Revert "Revert "Revert "[DNM]jf: Use the closed source camera solution [1/2]"""
    This reverts commit 6286b8433adb49ced58e9f12fed2520090a2fbc6.

--- jf-common.mk
+++ jf-common.mk
@@ -179,8 +179,9 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
-	camera.device@1.0-impl-legacy
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl \
+    camera.msm8960
 
 # RenderScript
 PRODUCT_PACKAGES += \
