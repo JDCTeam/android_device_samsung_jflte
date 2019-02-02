68492a196081ebec04a70eb706bcdfddfdc78dfe
dkati <dkatikaridis@gmail.com>
2019-02-02 11:45:57 +0200

    jf: Declare EGL's cache and key size

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -144,6 +144,16 @@ TARGET_NO_RPC := true
 # Allow suspend in charge mode
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
+
 # RIL
 BOARD_PROVIDES_LIBRIL := true
 TARGET_RIL_VARIANT := caf
