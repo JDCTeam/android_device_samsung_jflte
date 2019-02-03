b930a7a871caa4d2df92bf21eea188c811fdf069
dkati <dkatikaridis@gmail.com>
2019-02-03 22:01:03 +0200

    prop: Reenable hwui props from Stock

--- system_prop.mk
+++ system_prop.mk
@@ -43,7 +43,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=420 \
     ro.opengles.version=196608 \
     ro.qualcomm.cabl=0 \
-    ro.hwui.text_large_cache_height=2048
+    ro.hwui.texture_cache_size=50 \
+	ro.hwui.layer_cache_size=34 \
+	ro.hwui.path_cache_size=10 \
+	ro.hwui.shape_cache_size=4 \
+	ro.hwui.gradient_cache_size=2 \
+	ro.hwui.drop_shadow_cache_size=6 \
+	ro.hwui.text_small_cache_width=2048 \
+	ro.hwui.text_small_cache_height=2048 \
+	ro.hwui.text_large_cache_width=4096 \
+	ro.hwui.text_large_cache_height=4096
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
