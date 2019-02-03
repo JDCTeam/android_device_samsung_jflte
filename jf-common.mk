ca300421b581a377746417569cfe7472f37a3b60
dkati <dkatikaridis@gmail.com>
2019-02-03 13:49:40 +0200

    jf: Adapt hwaddrs for jflte

--- jf-common.mk
+++ jf-common.mk
@@ -208,7 +208,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service \
-    libbt-vendor
+    libbt-vendor \
+    hwaddrs
 
 # USB
 PRODUCT_PACKAGES += \
