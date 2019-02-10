9a376da09f96d04223e66983d1c69d53163811a2
dkati <dkatikaridis@gmail.com>
2019-02-10 12:24:50 +0200

    jf: bluetooth: Clean up configs/vnd/props
    again

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -21,7 +21,5 @@
 
 #define BTM_DEF_LOCAL_NAME   "Samsung Galaxy S IV"
 
-#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
-#define BTM_WBS_INCLUDED TRUE        /* Enable WBS */
 #define BTIF_HF_WBS_PREFERRED FALSE /* Don't default to WBS */
 #endif
