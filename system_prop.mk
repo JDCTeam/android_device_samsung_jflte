553609918bc95926c150fb0d6ea6c511ee895137
dkati <dkatikaridis@gmail.com>
2019-02-11 17:55:04 +0200

    jf: props: Add whatsapp to camera.hal1.packagelist

--- system_prop.mk
+++ system_prop.mk
@@ -56,7 +56,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    camera2.portability.force_api=1
+    camera2.portability.force_api=1 \
+    camera.hal1.packagelist=com.whatsapp
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
