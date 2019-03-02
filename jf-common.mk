570566e9e73c896d08fa8cd3e819c5bee58de268
dkati <dkatikaridis@gmail.com>
2019-03-02 20:20:13 +0200

    jf: ril : Update our buildables and change qmuxd class space

--- jf-common.mk
+++ jf-common.mk
@@ -275,6 +275,11 @@ PRODUCT_PACKAGES += \
     loki_bootloaders \
     recovery-transform.sh
 
+# Qcom RIL
+PRODUCT_PACKAGES += \
+    libcnefeatureconfig \
+    librmnetctl
+
 # Camera
 PRODUCT_PACKAGES += \
     Snap
