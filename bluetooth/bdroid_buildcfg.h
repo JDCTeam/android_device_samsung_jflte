5b3e5a05babe58309a4ab56408a710850233ddfb
dkati <dkatikaridis@gmail.com>
2019-03-08 19:54:40 +0200

    Fix compilation

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -20,7 +20,6 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME   "Samsung Galaxy S IV"
-
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
 #define BTIF_HF_WBS_PREFERRED FALSE  /* Use WBS    */
