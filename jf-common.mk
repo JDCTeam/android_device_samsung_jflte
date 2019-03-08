721feee4e4bdf8522743301191d31f1db8661509
dkati <dkatikaridis@gmail.com>
2019-03-08 22:29:32 +0200

    jf: Stop shipping bt_vendor.conf

--- jf-common.mk
+++ jf-common.mk
@@ -125,8 +125,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth firmware
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bluetooth/bcm4335_prepatch.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/bcm4335_prepatch.hcd \
-    $(LOCAL_PATH)/configs/bcmdhd.cal:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd.cal \
-    $(LOCAL_PATH)/bluetooth/bt_vendor.conf:/system/etc/bluetooth/bt_vendor.conf
+    $(LOCAL_PATH)/configs/bcmdhd.cal:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd.cal
 
 # Filesystem tools
 PRODUCT_PACKAGES += \
