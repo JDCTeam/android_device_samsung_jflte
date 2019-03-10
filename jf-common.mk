8734e4d924cecc5fcf7da1a6e486e3292e989444
dkati <dkatikaridis@gmail.com>
2019-03-10 12:51:44 +0200

    jf: Build netd 1.0 HAL and netutils-wrapper 1.0

--- jf-common.mk
+++ jf-common.mk
@@ -197,6 +197,11 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# Netutils
+PRODUCT_PACKAGES += \
+    android.system.net.netd@1.0 \
+    netutils-wrapper-1.0
+    
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
