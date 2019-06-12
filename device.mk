ea27cf29e4084d860fb8549be2e3cdb1599a6d08
jhenrique09 <jhsv09@gmail.com>
2019-06-12 11:44:13 +0100

    safetynet bypass hack
    Special credits to: @jhenrique09

--- device.mk
+++ device.mk
@@ -86,7 +86,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.qcom.usb.rc:root/init.qcom.usb.rc \
     $(LOCAL_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc \
     $(LOCAL_PATH)/rootdir/init.spectrum.sh:root/init.spectrum.sh \
-    $(LOCAL_PATH)/rootdir/init.spectrum.rc:root/init.spectrum.rc
+    $(LOCAL_PATH)/rootdir/init.spectrum.rc:root/init.spectrum.rc \
+    $(LOCAL_PATH)/rootdir/init.safailnet.rc:root/init.safailnet.rc
 
 
 # Time zone data for recovery
