25c73ca582301a3df7d3728b612883fd22395907
Arne Coucheron <arco68@gmail.com>
2019-06-15 23:46:39 +0000

    jf-common: Update Makefile guards and symlink creation
    * Add VIDC firmware symlinks.
    * Move symlinks to /vendor.
    * Guard makefile with samsung vendor and msm8960 chipset.
    * Remove old and incorrect makefile comments.
    
    Change-Id: Ic14f104917c7bba4a372224ceb74573894590498
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2012 The Android Open-Source Project
+# Copyright (C) 2013 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,23 +11,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-
-# WARNING: Everything listed here will be built on ALL platforms,
-# including x86, the emulator, and the SDK.  Modules must be uniquely
-# named (liblights.tuna), and must build everywhere, or limit themselves
-# to only building on ARM if they include assembly. Individual makefiles
-# are responsible for having their own logic, for fine-grained control.
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter jactivelte jflteatt jfltespr jfltetmo jfltevzw jfltexx jfltecan jflteusc jfltecri jfltecsp jfltezm jftddxx jfltetfnatt,$(TARGET_DEVICE)),)
+ifeq ($(BOARD_VENDOR),samsung)
+ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
+ifneq ($(filter jactivelte jflteatt jfltespr jfltetmo jfltevzw \
+                jfltexx jfltecan jflteusc jfltecri jfltecsp \
+                jfltezm jftddxx jfltetfnatt,$(TARGET_DEVICE)),)
 
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
-include $(CLEAR_VARS)
-
-# Create /firmware-mdm links
 FIRMWARE_MDM_IMAGES := \
     acdb.mbn \
     apps.mbn \
@@ -39,7 +32,7 @@ FIRMWARE_MDM_IMAGES := \
     sbl1.mbn \
     sbl2.mbn
 
-FIRMWARE_MDM_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_MDM_IMAGES)))
+FIRMWARE_MDM_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(FIRMWARE_MDM_IMAGES)))
 $(FIRMWARE_MDM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -48,13 +41,11 @@ $(FIRMWARE_MDM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MDM_SYMLINKS)
 
-
-# Create /firmware links
 FIRMWARE_IMAGES := \
     q6.b00 q6.b01 q6.b03 q6.b04 q6.b05 q6.b06 q6.mdt \
     tzapps.b00 tzapps.b01 tzapps.b02 tzapps.b03 tzapps.mdt
 
-FIRMWARE_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_IMAGES)))
+FIRMWARE_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(FIRMWARE_IMAGES)))
 $(FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Firmware link: $@"
 	@mkdir -p $(dir $@)
@@ -63,4 +54,18 @@ $(FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_SYMLINKS)
 
+VIDC_IMAGES := \
+    vidc.b00 vidc.b01 vidc.b02 vidc.b03 vidc.mdt
+
+VIDC_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(VIDC_IMAGES)))
+$(VIDC_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VIDC_SYMLINKS)
+
+endif
+endif
 endif
