1c6615540376e60e9d8057e85fc83a988f56bcf2
Arne Coucheron <arco68@gmail.com>
2019-06-16 05:26:59 +0000

    jf-common: configs: thermal: Redo based on latest jfltexx stock
    Change-Id: Ib4cc2b7b7762cc351b633605fcdac69c8cc48b6a
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>

--- jf-common.mk
+++ jf-common.mk
@@ -230,7 +230,7 @@ $(call inherit-product, device/samsung/jf-common/system_prop.mk)
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine-8064ab.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8064ab.conf
+    $(LOCAL_PATH)/configs/thermald.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermald.conf
 
 # Touch
 PRODUCT_PACKAGES += \
