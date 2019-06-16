6ead4deeffbfe9106b4028f8d6e2408378b1d769
Arne Coucheron <arco68@gmail.com>
2019-06-16 05:17:37 +0000

    jf-common: init: Add custom vendor init
    * Kanged from serrano.
    
    Change-Id: I0a128571c60be1635897f5acfe207a71a598ae95
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -156,6 +156,10 @@ BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
 TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
     /system/vendor/bin/hw/android.hardware.sensors@1.0-service.jf=22
 
+# Vendor Init
+TARGET_INIT_VENDOR_LIB := libinit_jf
+TARGET_RECOVERY_DEVICE_MODULES := libinit_jf
+
 # Wifi module
 BOARD_WLAN_DEVICE := bcmdhd
 BOARD_HAVE_SAMSUNG_WIFI := true
---
+++ init/init_jf.cpp
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+
+#include <android-base/file.h>
+#include <android-base/logging.h>
+#include <android-base/properties.h>
+#include <android-base/strings.h>
+
+#include "vendor_init.h"
+
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+using android::base::GetProperty;
+using android::base::ReadFileToString;
+using android::base::Trim;
+
+#define MODEL_NAME_LEN 5
+#define SERIAL_NUMBER_FILE "/efs/FactoryApp/serial_no"
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void property_override_dual(char const system_prop[], char const vendor_prop[], char const value[])
+{
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
+}
+
+void vendor_load_properties()
+{
+    const std::string bootloader = GetProperty("ro.bootloader", "");
+    const std::string platform = GetProperty("ro.board.platform", "");
+
+    char const *serial_number_file = SERIAL_NUMBER_FILE;
+    std::string serial_number;
+
+    if (platform != ANDROID_TARGET)
+        return;
+
+    if (ReadFileToString(serial_number_file, &serial_number)) {
+        serial_number = Trim(serial_number);
+        property_override("ro.serialno", serial_number.c_str());
+    }
+
+    const std::string device = android::base::GetProperty("ro.product.device", "");
+    LOG(INFO) << "Found bootloader " << bootloader.c_str() << ". " << "Setting build properties for " << device.c_str() << ".\n";
+}
