43a40a9db73d8e890e03cd054be75bf3c04e3438
Arne Coucheron <arco68@gmail.com>
2019-06-16 06:16:07 +0000

    jf-common: Build composer/memtrack HALs respective -service's
    * Also no longer build libgenlock, as it is not needed.
    
    Change-Id: I3a731bef45f6ad6f629501269d650dff1fc46547

--- jf-common.mk
+++ jf-common.mk
@@ -97,17 +97,15 @@ PRODUCT_PACKAGES += \
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
-    android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     copybit.msm8960 \
     gralloc.msm8960 \
     hwcomposer.msm8960 \
-    libgenlock \
     memtrack.msm8960
 
 # Doze
