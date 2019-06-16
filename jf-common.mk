7ea3881f7faf487340f1681a93eff7fd6d2e2131
Nolen Johnson <johnsonnolen@gmail.com>
2019-06-16 05:47:01 +0000

    jf-common: Build dynamically sized boot animation
    Change-Id: Ib5c4e4c2cacd7898e1a4c7fe2c0f159d537672a0

--- jf-common.mk
+++ jf-common.mk
@@ -58,10 +58,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
-
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 
 # Audio
