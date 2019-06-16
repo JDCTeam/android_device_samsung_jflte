eb8533ccc1904b40bbc42f2430e8ae31405202b8
Nolen Johnson <johnsonnolen@gmail.com>
2019-06-16 06:21:53 +0000

    jf-common: Build the HIDL DRM HAL
    Change-Id: Ib485bb53ddbf8a24576788299004bc6d1bdfb211

--- jf-common.mk
+++ jf-common.mk
@@ -112,6 +112,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SamsungDoze
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
+
 # Filesystem tools
 PRODUCT_PACKAGES += \
     e2fsck_static \
