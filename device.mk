c601f76e83d24c5beee83d47e1824a22995c2286
Amit Pundir <amit.pundir@linaro.org>
2019-07-08 22:17:09 +0530

    jf-common: Add gatekeeper software HIDL service.
    Use the default software implementation of gatekeeper.
    
    Change-Id: Id696752ad78047155cad6a5dafe7ca1b4fe86345
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- device.mk
+++ device.mk
@@ -190,6 +190,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-service.software
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.samsung
