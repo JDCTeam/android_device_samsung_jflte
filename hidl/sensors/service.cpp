26e6a8e147930af17978afae55bc7e1170b2266b
dkati <dkatikaridis@gmail.com>
2019-07-13 14:54:47 +0300

    jflte: Update our HIDL from gerrit

--- hidl/sensors/service.cpp
+++ hidl/sensors/service.cpp
@@ -21,6 +21,7 @@
 #ifdef ARCH_ARM_32
 #include <hwbinder/ProcessState.h>
 #endif
+
 using android::hardware::sensors::V1_0::ISensors;
 using android::hardware::defaultPassthroughServiceImplementation;
 
@@ -28,7 +29,6 @@ int main() {
 #ifdef ARCH_ARM_32
     android::hardware::ProcessState::initWithMmapSize((size_t)8192);
 #endif
-
     /* Sensors framework service needs at least two threads.
      * One thread blocks on a "poll"
      * The second thread is needed for all other HAL methods.
