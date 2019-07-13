4d3a85e67d21d5a05dc87c73404a1388fa19e623
dkati <dkatikaridis@gmail.com>
2019-07-13 17:31:19 +0300

    props: Graphics: Remove unused props
    vulkan one actually breaks our screen

--- props/Graphics.mk
+++ props/Graphics.mk
@@ -2,7 +2,6 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=430 \
     ro.opengles.version=196608 \
-    ro.qualcomm.cabl=0 \
     ro.hwui.texture_cache_size=50 \
 	ro.hwui.layer_cache_size=34 \
 	ro.hwui.path_cache_size=10 \
@@ -13,12 +12,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.hwui.text_small_cache_height=2048 \
 	ro.hwui.text_large_cache_width=4096 \
 	ro.hwui.text_large_cache_height=4096 \
-	persist.hwc.mdpcomp.enable=true \
-	persist.graphics.vulkan.disable=true \
 	debug.cpurend.vsync=false \
-	debug.sf.latch_unsignaled=1 \
-	debug.sf.disable_backpressure=1 \
-	debug.sf.recomputecrop=0 \
 	debug.composition.type=c2d \
     debug.hwui.use_buffer_age=false
-
