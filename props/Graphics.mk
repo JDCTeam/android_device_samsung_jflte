084f1e310717c20ca8f07f0e74f6294381169029
Buff99 <buffbuffster@gmail.com>
2019-08-10 00:57:12 +0100

    JF:props- prettify graphics.mk

--- props/Graphics.mk
+++ props/Graphics.mk
@@ -1,23 +1,25 @@
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=430 \
-    ro.opengles.version=196608 \
-    ro.hwui.texture_cache_size=50 \
+	debug.composition.type=c2d \
+	debug.cpurend.vsync=false \
+	debug.hwui.use_buffer_age=false \
+	ro.hwui.drop_shadow_cache_size=6 \
+	ro.hwui.gradient_cache_size=2 \
 	ro.hwui.layer_cache_size=34 \
 	ro.hwui.path_cache_size=10 \
 	ro.hwui.shape_cache_size=4 \
-	ro.hwui.gradient_cache_size=2 \
-	ro.hwui.drop_shadow_cache_size=6 \
-	ro.hwui.text_small_cache_width=2048 \
-	ro.hwui.text_small_cache_height=2048 \
-	ro.hwui.text_large_cache_width=4096 \
 	ro.hwui.text_large_cache_height=4096 \
-	debug.cpurend.vsync=false \
-	debug.composition.type=c2d \
-    debug.hwui.use_buffer_age=false \
-	persist.hwc.mdpcomp.enable=true \
-	persist.graphics.vulkan.disable=true \
-	debug.cpurend.vsync=false \
-	debug.sf.latch_unsignaled=1 \
+	ro.hwui.text_large_cache_width=4096 \
+	ro.hwui.text_small_cache_height=2048 \
+	ro.hwui.text_small_cache_width=2048 \
+	ro.hwui.texture_cache_size=50 \
+	ro.opengles.version=196608 \
+	ro.sf.lcd_density=430
+
+# Experimental
+PRODUCT_PROPERTY_OVERRIDES += \
 	debug.sf.disable_backpressure=1 \
-	debug.sf.recomputecrop=0
+	debug.sf.latch_unsignaled=1 \
+	debug.sf.recomputecrop=0 \
+	persist.graphics.vulkan.disable=true \
+	persist.hwc.mdpcomp.enable=true
