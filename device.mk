034be42288106c8468610bd2decdc1ceffbaeaf4
dkati <dkatikaridis@gmail.com>
2019-08-14 16:30:06 +0300

    Cosmetic commit

--- device.mk
+++ device.mk
@@ -129,10 +129,7 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
-    libqcomvoiceprocessing
-
-# TinyAlsa utils
-PRODUCT_PACKAGES += \
+    libqcomvoiceprocessing \
     tinyplay \
     tinycap \
     tinymix \
