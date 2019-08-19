e5f36bef75db6d914b972d783ba22c2b0a85e8ce
dkati <dkatikaridis@gmail.com>
2019-08-19 14:38:21 +0300

    jflte: Fix health service build

--- device.mk
+++ device.mk
@@ -224,7 +224,7 @@ PRODUCT_PACKAGES += \
 
 # Health HAL
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.0-service.jf
 
 # Lights
 PRODUCT_PACKAGES += \
