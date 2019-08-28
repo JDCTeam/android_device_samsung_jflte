772f3ee72b6182ba414a0f5237bce5ad9f453ba5
dkati <dkatikaridis@gmail.com>
2019-08-28 20:29:14 +0300

    lineage: Clean up

--- rr_jflte.mk
+++ rr_jflte.mk
@@ -22,31 +22,24 @@ $(call inherit-product, device/samsung/jflte/device.mk)
 # Inherit some Resurrection Remix stuff.
 $(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
-# Boot animation
-TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_BOOT_ANIMATION_RES := 1080
-
 PRODUCT_NAME := rr_jflte
 PRODUCT_DEVICE := jflte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
 PRODUCT_RELEASE_NAME := jflte
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rr.device=jflte
+# Boot animation res
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=jfltexx \
+    TARGET_DEVICE=jfltexx \
     PRIVATE_BUILD_DESC="jfltexx-user 5.0.1 LRX22C I9505XXUHQK1 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := samsung/jfltexx/jflte:5.0.1/LRX22C/I9505XXUHQK1:user/release-keys
-VENDOR_BUILD_FINGERPRINT := samsung/jfltexx/jflte:5.0.1/LRX22C/I9505XXUHQK1:user/release-keys
-
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="GT-I9505"
