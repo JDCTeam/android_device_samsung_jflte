82c5632fbb3c8f461978ac6b205df9d54c560e3f
dkati <dkatikaridis@gmail.com>
2019-08-28 20:29:14 +0300

    lineage: Clean up

--- gzosp_jflte.mk
+++ gzosp_jflte.mk
@@ -25,30 +25,29 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 # Inherit some Gzosp stuff.
 $(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
 
+PRODUCT_NAME := gzosp_jflte
+PRODUCT_DEVICE := jflte
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MODEL := GT-I9505
+
 # Boot animation res
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := gzosp_jflte
-PRODUCT_DEVICE := jflte
-PRODUCT_BRAND := samsung
-PRODUCT_MANUFACTURER := Samsung
+PRODUCT_RELEASE_NAME := jflte
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=jfltexx \
+    TARGET_DEVICE=jfltexx \
     PRIVATE_BUILD_DESC="jfltexx-user 5.0.1 LRX22C I9505XXUHQK1 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := samsung/jfltexx/jflte:5.0.1/LRX22C/I9505XXUHQK1:user/release-keys
-VENDOR_BUILD_FINGERPRINT := samsung/jfltexx/jflte:5.0.1/LRX22C/I9505XXUHQK1:user/release-keys
-
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="GT-I9505"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DEVICE_MAINTAINERS="Buff99 & JDCTeam"
