9f36d14cc9d38f9b9be4b318d24637a6df161f1f
dkati <dkatikaridis@gmail.com>
2019-08-28 19:48:26 +0300

    jf: alphabetical short

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,38 +29,17 @@ DEVICE_PATH := device/samsung/jflte
 # inherit from qcom-common
 -include device/samsung/qcom-common/BoardConfigCommon.mk
 
-# Includes
-TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
-
-# The first api level the device has commercially launched on
-PRODUCT_SHIPPING_API_LEVEL := 17
-
-# HIDL
-# Manifests
-DEVICE_FRAMEWORK_MANIFEST_FILE := system/libhidl/vintfdata/manifest_healthd_exclude.xml
-DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/vendor_interface/manifest.xml
-DEVICE_MATRIX_FILE := $(DEVICE_PATH)/vendor_interface/compatibility_matrix.xml
-
-# Set default locale
-PRODUCT_LOCALES := en-GB
-
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
-
 # Platform
 TARGET_BOARD_PLATFORM := msm8960
+
+# Architecture
 TARGET_CPU_VARIANT := krait
 
 # Binder
 TARGET_USES_64_BIT_BINDER := true
 
-# Legacy
- TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=22 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
@@ -79,28 +58,6 @@ TARGET_KERNEL_VARIANT_CONFIG := jf_eur_defconfig
 # KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/gcc/linux-x86/arm/arm-linux-7.1.x-gnueabi/bin
 # TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-Samsung-linux-gnueabi-
 
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := MSM8960
-
-# Recovery
-LZMA_RAMDISK_TARGETS := recovery
-TARGET_RECOVERY_DENSITY := hdpi
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
-
-# Filesystem
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1181114368
-BOARD_SYSTEMIMAGE_JOURNAL_SIZE := 0
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 28551290624
-BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_CACHEIMAGE_PARTITION_SIZE := 2170552320
-BOARD_FLASH_BLOCK_SIZE := 131072
-TARGET_USES_MKE2FS := true
-# Releasetools
-TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
-
 # Audio
 AUDIO_FEATURE_ENABLED_COMPRESS_VOIP := true
 AUDIO_FEATURE_ENABLED_FLUENCE := true
@@ -116,11 +73,25 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 BOARD_CUSTOM_BT_CONFIG := $(DEVICE_PATH)/bluetooth/vnd_jf.txt
 BOARD_HAVE_BLUETOOTH_BCM := true
 
-# Legacy blobs
-TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
-    /system/bin/mediaserver=22 \
-    /system/vendor/bin/mm-qcamera-daemon=22 \
-    /system/vendor/bin/hw/android.hardware.sensors@1.0-service.jf=22
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Includes
+TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
+
+# HIDL
+# Manifests
+DEVICE_FRAMEWORK_MANIFEST_FILE := system/libhidl/vintfdata/manifest_healthd_exclude.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/vendor_interface/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/vendor_interface/compatibility_matrix.xml
+
+# Set default locale
+PRODUCT_LOCALES := en-GB
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Camera
 TARGET_HAS_LEGACY_CAMERA_HAL1 := true
@@ -130,6 +101,71 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 TARGET_NEED_SAMSUNG_CAMERA_MODE := true
 TARGET_NEED_CAMERA_ZSL := true
 TARGET_DISPLAY_INSECURE_MM_HEAP := true
+
+TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
+    /system/bin/mediaserver=22 \
+    /system/vendor/bin/mm-qcamera-daemon=22 \
+    /system/vendor/bin/hw/android.hardware.sensors@1.0-service.jf=22
+
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+
+# Display
+TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x02000000U
+ARCH_ARM_HAVE_NEON := true
+USE_OPENGL_RENDERER := true
+TARGET_USES_ION := true
+
+# Always preopt extracted APKs to prevent extracting out of the APK
+# for gms modules.
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+
+# Exclude serif fonts for saving system.img size.
+EXCLUDE_SERIF_FONTS := true
+
+# GPS
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
+BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
+TARGET_NO_RPC := true
+
+# Legacy
+TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
+
+# Recovery
+LZMA_RAMDISK_TARGETS := recovery
+TARGET_RECOVERY_DENSITY := hdpi
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
+
+# Partitions
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1181114368
+BOARD_SYSTEMIMAGE_JOURNAL_SIZE := 0
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 28551290624
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_CACHEIMAGE_PARTITION_SIZE := 2170552320
+BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_USES_MKE2FS := true
+
+# Power
+TARGET_HAS_NO_POWER_STATS := true
+TARGET_HAS_NO_WLAN_STATS := true
+
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
+
+# Renderscript
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+
+# RIL
+BOARD_PROVIDES_LIBRIL := true
+TARGET_RIL_VARIANT := caf
+TARGET_USES_OLD_MNC_FORMAT := true
+
+# The first api level the device has commercially launched on
+PRODUCT_SHIPPING_API_LEVEL := 17
+
 
 # Enable dexpreopt to speed boot time
 WITH_DEXPREOPT := true
@@ -146,22 +182,26 @@ PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
 # Compile libhwui in performance mode
 HWUI_COMPILE_FOR_PERF := true
 
-# Display
-TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x02000000U
-ARCH_ARM_HAVE_NEON := true
-USE_OPENGL_RENDERER := true
-TARGET_USES_ION := true
+# Speed profile services and wifi-service to reduce RAM and storage.
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
-# Renderscript
-OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+# Vendor init
+TARGET_INIT_VENDOR_LIB := libinit_jflte
+TARGET_RECOVERY_DEVICE_MODULES := libinit_jflte
 
-# GPS
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
-BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
-TARGET_NO_RPC := true
+# Wifi module
+BOARD_WLAN_DEVICE := bcmdhd
+BOARD_HAVE_SAMSUNG_WIFI := true
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_${BOARD_WLAN_DEVICE}
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_${BOARD_WLAN_DEVICE}
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_BAND := 802_11_ABG
+WIFI_DRIVER_FW_PATH_AP := "/vendor/etc/wifi/bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/dhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA := "/vendor/etc/wifi/bcmdhd_sta.bin"
 
-# Allow suspend in charge mode
-BOARD_CHARGER_ENABLE_SUSPEND := true
 
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
@@ -173,45 +213,9 @@ MAX_EGL_CACHE_KEY_SIZE := 12*1024
 # of the device.
 MAX_EGL_CACHE_SIZE := 2048*1024
 
-# RIL
-BOARD_PROVIDES_LIBRIL := true
-TARGET_RIL_VARIANT := caf
-TARGET_USES_OLD_MNC_FORMAT := true
-
-# Power
-TARGET_HAS_NO_POWER_STATS := true
-TARGET_HAS_NO_WLAN_STATS := true
-
-# Vendor init
-TARGET_INIT_VENDOR_LIB := libinit_jflte
-TARGET_RECOVERY_DEVICE_MODULES := libinit_jflte
-
-# Exclude serif fonts for saving system.img size.
-EXCLUDE_SERIF_FONTS := true
-
 # Don't try to build and run all tests by default. Several tests have
 # dependencies on the framework.
 ANDROID_NO_TEST_CHECK := true
-
-# Speed profile services and wifi-service to reduce RAM and storage.
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
-
-# Always preopt extracted APKs to prevent extracting out of the APK
-# for gms modules.
-PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
-
-# Wifi module
-BOARD_HAVE_SAMSUNG_WIFI := true
-BOARD_WLAN_DEVICE := bcmdhd
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_${BOARD_WLAN_DEVICE}
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_${BOARD_WLAN_DEVICE}
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-WIFI_BAND := 802_11_ABG
-WIFI_DRIVER_FW_PATH_AP := "/vendor/etc/wifi/bcmdhd_apsta.bin"
-WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA := "/vendor/etc/wifi/bcmdhd_sta.bin"
 
 # Include SE policy
 include device/qcom/sepolicy-legacy/sepolicy.mk
