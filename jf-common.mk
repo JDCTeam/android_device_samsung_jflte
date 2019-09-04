289609d9733997743069d618da017e6d1ce0d24b
Erfan Abdi <erfangplus@gmail.com>
2019-09-04 15:20:41 +0200

    jf-common: Setup device soong namespace
    Change-Id: I2e9c0ab68a0fe86ba18e3955e2823b87d88e835a
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- jf-common.mk
+++ jf-common.mk
@@ -23,6 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_k.mk)
 # Get non-open-source specific aspects if available
 $(call inherit-product-if-exists, vendor/samsung/jf-common/jf-common-vendor.mk)
 
+# Soong
+PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
