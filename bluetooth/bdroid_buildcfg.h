81452026558c43585cd6537fdc9a689a02bc2e74
Arne Coucheron <arco68@gmail.com>
2019-09-15 00:07:46 +0200

    jf-common: bluetooth: Remove BLE_PRIVACY_SPT
    Change-Id: Ic9b3282cd43a07779c31c932877295d1716f697b

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -25,7 +25,6 @@
 #define SC_MODE_INCLUDED FALSE
 #define SDP_DEBUG FALSE
 #define BTSNOOP_MEM FALSE
-#define BLE_PRIVACY_SPT FALSE
 #define BTM_WBS_INCLUDED TRUE /* Enable WBS */
 #define BTIF_HF_WBS_PREFERRED FALSE /* Do not prefer WBS */
 
