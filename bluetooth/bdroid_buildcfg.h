62d10c7ba38d4c21e49842205dcae32900b28a63
dkati <dkatikaridis@gmail.com>
2019-09-19 21:08:06 +0300

    bluetooth: Sync with LG G2's

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -21,11 +21,9 @@
 
 #define BTM_DEF_LOCAL_NAME   "Samsung Galaxy S IV"
 
-#define BTA_DISABLE_DELAY 100 /* in milliseconds */
-#define SC_MODE_INCLUDED FALSE
-#define SDP_DEBUG FALSE
-#define BTSNOOP_MEM FALSE
-#define BTM_WBS_INCLUDED TRUE /* Enable WBS */
-#define BTIF_HF_WBS_PREFERRED FALSE /* Do not prefer WBS */
+#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
+/* Allow car handsfree setup to work (BMW and Mercedes) */
+#define BTM_WBS_INCLUDED TRUE        /* Enable WBS */
+#define BTIF_HF_WBS_PREFERRED FALSE  /* Don't default to WBS */
 #endif
