c2c12a0d7b87e97416a3da23087853ec90909e38
Arne Coucheron <arco68@gmail.com>
2019-09-23 01:43:13 +0200

    jf-common: Restore Miracast support
    Change-Id: I5f7790fe55d2d533f3880a964db72528b4c12646

--- props/Graphics.mk
+++ props/Graphics.mk
@@ -1,25 +1,28 @@
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
-	debug.composition.type=c2d \
-	debug.cpurend.vsync=false \
-	debug.hwui.use_buffer_age=false \
-	ro.hwui.drop_shadow_cache_size=6 \
-	ro.hwui.gradient_cache_size=2 \
+	ro.sf.lcd_density=430 \
+	ro.opengles.version=196608 \
+	ro.hwui.texture_cache_size=50 \
 	ro.hwui.layer_cache_size=34 \
 	ro.hwui.path_cache_size=10 \
 	ro.hwui.shape_cache_size=4 \
-	ro.hwui.text_large_cache_height=4096 \
-	ro.hwui.text_large_cache_width=4096 \
-	ro.hwui.text_small_cache_height=2048 \
+	ro.hwui.gradient_cache_size=2 \
+	ro.hwui.drop_shadow_cache_size=6 \
 	ro.hwui.text_small_cache_width=2048 \
-	ro.hwui.texture_cache_size=50 \
-	ro.opengles.version=196608 \
-	ro.sf.lcd_density=430
+	ro.hwui.text_small_cache_height=2048 \
+	ro.hwui.text_large_cache_width=4096 \
+	ro.hwui.text_large_cache_height=4096 \
+	debug.cpurend.vsync=false \
+	debug.composition.type=c2d \
+	debug.hwui.use_buffer_age=false \
+    debug.hwui.use_buffer_age=false \
+    persist.debug.wfd.enable=1 \
+    persist.sys.wfd.virtual=0
 
 # Experimental
 PRODUCT_PROPERTY_OVERRIDES += \
-	debug.sf.disable_backpressure=1 \
-	debug.sf.latch_unsignaled=1 \
-	debug.sf.recomputecrop=0 \
+	persist.hwc.mdpcomp.enable=true \
 	persist.graphics.vulkan.disable=true \
-	persist.hwc.mdpcomp.enable=true
+	debug.sf.latch_unsignaled=1 \
+	debug.sf.disable_backpressure=1 \
+	debug.sf.recomputecrop=0
