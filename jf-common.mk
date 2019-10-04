6d94c38fc9088b83efd26579ca9755eea3349c2d
Bruno Martins <bgcngm@gmail.com>
2019-10-04 23:20:42 +0100

    jf-common: Switch to legacy Wi-Fi service
    Change-Id: I690bc80349e47907a29a89b8a6f96659f1c61aba

--- jf-common.mk
+++ jf-common.mk
@@ -285,7 +285,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.legacy \
     macloader \
     hostapd \
     libwpa_client \
