d3dd1bc006545f0503b61a9cc7d46fc372a409fe
Arne Coucheron <arco68@gmail.com>
2019-10-08 03:48:19 +0200

    [DNM] jf-common: Disable sepolicy
    Change-Id: I87b67a703fd4377856888575e5024af621ab8104

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -142,8 +142,9 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)/releasetools
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 
 # SELinux
-include device/qcom/sepolicy-legacy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
+#include device/qcom/sepolicy-legacy/sepolicy.mk
+#BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy_tmp
 
 # Sensors
 TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
