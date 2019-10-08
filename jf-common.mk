a9dd94a1d704f10bc6ce744f1e62670e0ef55550
Arne Coucheron <arco68@gmail.com>
2019-10-09 00:38:18 +0200

    jf-common: gps: Use gps.conf with our changes
    Change-Id: I7f307e04daa88cdeb6c45d3dfbb2ed0715d91610

--- jf-common.mk
+++ jf-common.mk
@@ -159,10 +159,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
     android.hardware.gnss@1.0-service \
+    gps.conf \
     gps.msm8960
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
 # IPv6 tethering
