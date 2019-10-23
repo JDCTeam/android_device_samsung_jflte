bf2511b90c59e95f6b66a65413dc37d410aacd00
dkati <dkatikaridis@gmail.com>
2019-10-24 00:45:16 +0300

    jflte: Clean up conditional subdir calls

--- Android.mk
+++ Android.mk
@@ -11,14 +11,19 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+
+# WARNING: Everything listed here will be built on ALL platforms,
+# including x86, the emulator, and the SDK.  Modules must be uniquely
+# named (liblights.tuna), and must build everywhere, or limit themselves
+# to only building on ARM if they include assembly. Individual makefiles
+# are responsible for having their own logic, for fine-grained control.
 
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_VENDOR),samsung)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
-ifneq ($(filter jactivelte jflteatt jfltespr jfltetmo jfltevzw \
-                jfltexx jfltecan jflteusc jfltecri jfltecsp \
-                jfltezm jftddxx jfltetfnatt jfvelte,$(TARGET_DEVICE)),)
+ifneq ($(filter jactivelte jflte,$(TARGET_DEVICE)),)
 
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/lineage_jflte.mk
 
-	COMMON_LUNCH_CHOICES := \
-    	lineage_jflte-userdebug \
-    	lineage_jflte-eng
+COMMON_LUNCH_CHOICES := \
+    lineage_jflte-userdebug \
+    lineage_jflte-eng
