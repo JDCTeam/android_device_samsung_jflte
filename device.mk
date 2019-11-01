1b8c12f2bece55c690734e5c8f10b89f55119ba2
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-01 07:19:07 -0500

    jf-common: Exclude lineage-sdk overlays from RRO
    * lineage-sdk overlays don't work with RRO
    
    Change-Id: Iff826faf1864388066a488acc9682636b98d1098

--- device.mk
+++ device.mk
@@ -36,6 +36,7 @@ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/lineage-sdk
 
 # Always preopt extracted APKs to prevent extracting out of the APK
 # for gms modules.
