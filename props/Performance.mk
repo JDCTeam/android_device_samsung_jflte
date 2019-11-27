43d6ed19c88bd1d08af641ea6465ce40cd01acf4
dkati <dkatikaridis@gmail.com>
2019-11-27 22:28:16 +0200

    props: Performance: Turn off jit profiles and use speed ones

--- props/Performance.mk
+++ props/Performance.mk
@@ -10,7 +10,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	dalvik.vm.heapsize=512m \
 	dalvik.vm.heaptargetutilization=0.75 \
 	dalvik.vm.heapminfree=4m \
-	dalvik.vm.heapmaxfree=8m
+	dalvik.vm.heapmaxfree=8m \
+	dalvik.vm.usejitprofiles=false \
+	pm.dexopt.bg-dexopt=speed \
+	pm.dexopt.ab-ota=speed
 
 # QC Perf
 PRODUCT_PROPERTY_OVERRIDES += \
