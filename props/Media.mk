04f687d0383d81d3a972a7197f04af317a10f11f
dkati <dkatikaridis@gmail.com>
2020-04-01 15:32:07 +0300

    props: media: Set volume steps to 45

--- props/Media.mk
+++ props/Media.mk
@@ -13,3 +13,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     camera2.portability.force_api=1 \
     media.stagefright.legacyencoder=true \
 	media.stagefright.less-secure=true
+
+# Volume
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.config.media_vol_steps=45
