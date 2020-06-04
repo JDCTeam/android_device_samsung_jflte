a6bbf15cbe42e0f3fda6fb21966a5ef75fe775ce
Buff99 <buffbuffster@gmail.com>
2020-06-04 02:01:08 +0100

    JF:props - split audio from media

--- props/Media.mk
+++ props/Media.mk
@@ -1,15 +1,17 @@
-# Media
+# Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-    audio.offload.disable=1 \
+    audio.offload.disable=true \
     persist.vendor.audio.fluence.speaker=true \
     persist.vendor.audio.fluence.voicecall=true \
     ro.vendor.audio.sdk.fluencetype=fluence \
     vendor.dedicated.device.for.voip=true \
-    vendor.voice.path.for.pcm.voip=true \
+    vendor.voice.path.for.pcm.voip=true
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
     debug.stagefright.ccodec=0 \
     debug.stagefright.omx_default_rank.sw-audio=1 \
     debug.stagefright.omx_default_rank=0
-
 # Volume
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.media_vol_steps=45
