decadafd5c46264e55e0568c4b0ad4c88d32b36c
Buff99 <buffbuffster@gmail.com>
2020-06-15 05:42:10 +0100

    JF: remove HEALTHD_USE_BATTERY_INFO

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,9 +41,6 @@ TARGET_CPU_VARIANT_RUNTIME := krait
 # Binder
 TARGET_USES_64_BIT_BINDER := true
 
-#Battery
-HEALTHD_USE_BATTERY_INFO := true
-
 #Pixel Charging animation
 USE_PIXEL_CHARGER_IMAGES=true
 
