5714a021cc41956e75d8aaf2002db505efa0f428
Buff99 <buffbuffster@gmail.com>
2020-08-08 01:04:15 +0100

    [DNM] JF - prune props - test pink shit

--- props/Security.mk
+++ props/Security.mk
@@ -2,10 +2,6 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.lineage.build.vendor_security_patch=2017-08-01
 
-# SU
-PRODUCT_PROPERTY_OVERRIDES += \
-	persist.sys.root_access=3
-
 # Debug
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.adb.secure=0 \
