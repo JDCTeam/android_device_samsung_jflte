5f10d2560d431cc7bfd108bdd73fbdf34005a626
Buff99 <buffbuffster@gmail.com>
2020-08-25 23:45:14 +0100

    JF:props - restore media and performance props

--- props/Performance.mk
+++ props/Performance.mk
@@ -11,5 +11,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Dex
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dex2oat-swap=false
-
+    dalvik.vm.dex2oat-swap=false \
+    pm.dexopt.install=quicken \
+    dalvik.vm.dex2oat-swap=false \
+    dalvik.vm.dex2oat-filter=quicken \
+    dalvik.vm.image-dex2oat-threads=4 \
+    dalvik.vm.image-dex2oat-filter=quicken \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=4m \
+    dalvik.vm.heapmaxfree=8m \
+    dalvik.vm.usejitprofiles=false \
+    pm.dexopt.bg-dexopt=speed \
+    pm.dexopt.ab-ota=speed
