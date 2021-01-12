1576fd99c716fecded09e2e2405adc634c9abda5
Arne Coucheron <arco68@gmail.com>
2021-01-12 05:45:19 +0100

    jf-common: Silence clang warnings during kernel compile
    clang-11: warning: argument unused during compilation:
    '-fuse-ld=lld' [-Wunused-command-line-argument]
    
    Change-Id: I66a629f3573e3fad8aa085ea115475f02d60a7ea

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,6 +37,10 @@ TARGET_BOARD_PLATFORM := msm8960
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_VARIANT_RUNTIME := krait
 
+# Kernel
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+
 # Binder
 TARGET_USES_64_BIT_BINDER := true
 
