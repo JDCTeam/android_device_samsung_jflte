9ae54ad0b874b7ec44ff92f299d096c7c777167a
dkati <dkatikaridis@gmail.com>
2022-03-17 20:33:12 +0200

    jf: power: Migrate to QTI

--- device.mk
+++ device.mk
@@ -260,8 +260,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
