791923451f777cc4360aa1d4ea3d9246484cdc14
dkati <dkatikaridis@gmail.com>
2022-03-17 20:13:41 +0200

    jf: Switch to new lights HAL

--- device.mk
+++ device.mk
@@ -201,7 +201,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.samsung
+    android.hardware.light-service.samsung
 
 # Loki
 PRODUCT_PACKAGES += \
