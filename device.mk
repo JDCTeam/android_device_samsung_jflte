5f35806f82c42d4272acf51b268ec604dd71ab20
dkati <dkatikaridis@gmail.com>
2022-03-17 20:29:18 +0200

    jf: Upgrade audio interfaces

--- device.mk
+++ device.mk
@@ -82,9 +82,9 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.service \
+    android.hardware.audio.effect@6.0-impl \
     audio.a2dp.default \
     audio.primary.msm8960 \
     audio.r_submix.default \
