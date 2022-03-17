c6eff527fac8897f1b818cd4fe08c37221d43828
dkati <dkatikaridis@gmail.com>
2022-03-17 20:18:30 +0200

    props: Drop secure adb

--- system_prop.mk
+++ system_prop.mk
@@ -96,7 +96,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Debug
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.adb.secure=0 \
     ro.debuggable=1 \
     persist.service.adb.enable=1 \
     persist.sys.usb.config=adb \
