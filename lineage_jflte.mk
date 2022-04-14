6c35d4bdacdb96392703fa57868049189d0109cd
dkati <dkatikaridis@gmail.com>
2022-04-14 22:15:51 +0300

    jflte: Enable Go configs

--- lineage_jflte.mk
+++ lineage_jflte.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/jflte/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_go_phone.mk)
 
 # Inherit some JDCTeam stuff.
 $(call inherit-product, vendor/jdc/config/common.mk)
