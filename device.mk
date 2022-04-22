eb8cf2a4a92a38ad1742fb8dc26651b013a237ed
dkati <dkatikaridis@gmail.com>
2022-04-22 22:08:31 +0300

    jflte: Use the minimal widevine vintf service
    we use closed blob for widevine anyways

--- device.mk
+++ device.mk
@@ -163,9 +163,7 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.4-service.clearkey \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.0-service
 
 # HIDL
 PRODUCT_PACKAGES += \
