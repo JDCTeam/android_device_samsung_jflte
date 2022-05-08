f8027424e6f707c7b04c5662034490d1fb211d37
dkati <dkatikaridis@gmail.com>
2022-05-08 23:53:50 +0300

    jf: provide keymaster for vendor

--- device.mk
+++ device.mk
@@ -221,6 +221,8 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.0-service-qti.rc \
+    android.hardware.keymaster@4.1.vendor \
     android.hardware.keymaster@4.1-service
 
 # Gatekeeper
